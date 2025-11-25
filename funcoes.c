aa71178556cae2196c5c23310bce94aafafda53e
BronoSemRede <124004864+Brunoxd21@users.noreply.github.com>
2025-11-25 19:40:15 +0000

    Add files via upload

---
+++ funcoes.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include "funcoes.h"
+
+//----------------------------------------------------
+// Inicialização e carregamento de dados
+//----------------------------------------------------
+void configurarParque() {
+
+}
+
+void carregarTarifasDeFicheiro() {
+
+}
+
+void carregarEstacionamentosDeFicheiro() {
+
+}
+
+void inicializarSistema() {
+
+}
+
+//----------------------------------------------------
+// Menus
+//----------------------------------------------------
+void mostrarMenuPrincipal() {
+
+}
+
+void mostrarOcupacaoPisos() {
+
+}
+
+//----------------------------------------------------
+// Entrada de veículos
+//----------------------------------------------------
+int registarEntradaVeiculo() {
+
+    return 0;
+}
+
+int atribuirLugar(int piso) {
+
+    return 0;
+}
+
+void mostrarTicketEntrada(int numEntrada) {
+
+}
+
+//----------------------------------------------------
+// Saída de veículos
+//----------------------------------------------------
+int registarSaidaVeiculo() {
+
+    return 0;
+}
+
+float calcularValorAPagar(int numEntrada) {
+
+    return 0.0;
+}
+
+void mostrarTicketSaida(int numEntrada) {
+
+}
+
+void alterarSaida(int numEntrada) {
+
+}
+
+void anularSaida(int numEntrada) {
+
+}
+
+//----------------------------------------------------
+// Consulta, alteração e eliminação de registos
+//----------------------------------------------------
+void consultarEstacionamento(int numEntrada) {
+
+}
+
+void alterarEstacionamento(int numEntrada) {
+
+}
+
+void eliminarEstacionamento(int numEntrada) {
+
+}
+
+//----------------------------------------------------
+// Lugares indisponíveis
+//----------------------------------------------------
+void marcarLugarIndisponivel() {
+
+}
+
+void reverterLugarIndisponivel() {
+
+}
+
+//----------------------------------------------------
+// Mapa do piso
+//----------------------------------------------------
+void mostrarMapaPiso(int piso) {
+
+}
+
+//----------------------------------------------------
+// Persistência
+//----------------------------------------------------
+void guardarDadosEmBinario() {
+
+}
+
+void gravarEstacionamentosTexto() {
+
+}
+
+void gravarErros() {
+
+}
+
+//----------------------------------------------------
+// Listagens e paginação
+//----------------------------------------------------
+void listarEstacionamentos() {
+
+}
+
+void avancarPagina() {
+
+}
+
+void recuarPagina() {
+
+}
+
+void mostrarPagina(int pagina) {
+
+}
+
+void gravarListagemTXT() {
+
+}
+
+//----------------------------------------------------
+// Funcionalidades adicionais obrigatórias (3 à escolha)
+//----------------------------------------------------
+void funcionalidadeExtra1() {
+
+}
+
+void funcionalidadeExtra2() {
+
+}
+
+void funcionalidadeExtra3() {
+
+}
+
+//----------------------------------------------------
+// Extras opcionais (E1, E2, E3)
+//----------------------------------------------------
+void gerarGrafico() {
+
+}
+
+void gerarTabelaDinamica() {
+
+}
+
+void gerarCSV() {
+
+}
