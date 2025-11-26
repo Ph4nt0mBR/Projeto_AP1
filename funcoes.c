6f781912e66c09980b575daa9ea98ab306e8de85
BronoSemRede <124004864+Brunoxd21@users.noreply.github.com>
2025-11-26 22:43:36 +0000

    Add parking system initialization and tariff loading
    Implemented functions for initializing the parking system and loading tariffs from a file. Added input validation for parking configuration. (adoro a overview do github)

--- funcoes.c
+++ funcoes.c
@@ -4,12 +4,66 @@
 //----------------------------------------------------
 // Inicialização e carregamento de dados
 //----------------------------------------------------
-void configurarParque() {
+void configurarParque(Parque *p) {    //eu amanha documento isto melhor maltinha. (bruno)
 
-}
 
-void carregarTarifasDeFicheiro() {
+    do{
+        printf("Tamanho de pisos(1-5):\n");
+        scanf("%d", &p->pisos);
 
+    }while (p->pisos<1 || p->pisos > MAX_PISOS); //verifica se o numero de pisos e valido(maior q um, menor q 5
+        do{
+            printf("Numero de filas por piso (1-26): \n");
+            scanf("%d", &p->filasPorPiso);
+        } while (p->filasPorPiso < 1 || p->filasPorPiso > MAX_FILAS);
+
+        do{
+            printf("Numero de lugares por cada fila(1-50):\n");
+            scanf("%d", &p->lugaresPorFila);
+        } (p->lugaresPorFila < 1 || p->lugaresPorFila > MAX_LUGARES);
+
+} //acho q thats it. qualquer cena volta se ca no troublleshoot
+
+void carregarTarifasDeFicheiro(Sistema *s) {
+    FILE *f = fopen("Tarifario.txt", "r");
+    if (!f) {
+        printf("Erro ao abrir o ficheito Tarefario.txt.\n");
+        return;
+    }
+
+    s->totalTarifas = 0;
+    
+    char tipo;
+    char codigo[10];
+    char hInf[10];
+    char hSup[10];
+    char valorStr[20];
+    
+    while (fscanf(f, " %c %s %s %s %s", &tipo, codigo, hInf, hSup, valorStr) == 5)
+    {
+        
+        Tarifario t;
+        t.tipoTarifa = tipo;
+        strcpy(t.codigo, codigo);
+        
+        int hora, min;
+        
+        sscanf(hInf, "%d:%d", &hora, &min);
+        t.horaInf = hora * 100 + min;
+        
+        sscanf(hSup, "%d:%d", &hora, &min);
+        t.horaInf = hora * 100 + min;
+        valorStr[strcspn(valorStr,"€")] = "\0";
+        
+        t.valor = atof(valorStr);
+        
+        s->tarifas[s->totalTarifas++] = t;
+    }
+    
+    fclose(f);
+    
+    printf("Tarifas carregadas com sucesso: %d\n", s->totalTarifas);
+        
 }
 
 void carregarEstacionamentosDeFicheiro() {
@@ -173,3 +227,4 @@ void gerarTabelaDinamica() {
 void gerarCSV() {
 
 }
+
